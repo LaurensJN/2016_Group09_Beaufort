a86e608de4f81ef90c804ac7c5012c96e496fc56
PuckF <puckflikweert@gmail.com>
2016-12-16 11:44:09 +0100

    edit

--- SpatialDecision/spatial_decision_dockwidget.py
+++ SpatialDecision/spatial_decision_dockwidget.py
@@ -156,7 +156,7 @@ class SpatialDecisionDockWidget(QtGui.QDockWidget, FORM_CLASS):
 #######
     def openScenario(self,filename=""):
         scenario_open = False
-        scenario_file = os.path.join(self.plugin_dir,'Real_data','Rotterdam.qgs')
+        scenario_file = os.path.join(self.plugin_dir,'Real_data\SpatialDecision\qgisfile','rotterdam.qgs')
         # check if file exists
         if os.path.isfile(scenario_file):
             self.iface.addProject(scenario_file)
